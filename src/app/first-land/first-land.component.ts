b22e5a4f1aadf4e91887a15bdb4ee0b275a9d0e5
Kaur Ruukel <113232105+kaurruukel@users.noreply.github.com>
2024-01-02 21:37:43 +0200

    small

--- src/app/first-land/first-land.component.ts
+++ src/app/first-land/first-land.component.ts
@@ -38,6 +38,7 @@ export class FirstLandComponent {
                     this.goToMain();
                 },
                 error: (error: any) => {
+                    console.log(error);
                     this.goToMain();
                 },
             });
--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://lithium-f2g4.onrender.com',
+    apiUrl: 'http://localhost:3000',
 };
